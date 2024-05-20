aa1b54e95f7722990d9e33bae1a779f154736ae8
Shubham-2122 <152593580+Shubham-2122@users.noreply.github.com>
2024-05-20 16:38:25 +0530

    sd

---
+++ Advance JS/17testModule.js
@@ -0,0 +1,14 @@
+
+
+const Test = ()=>{
+
+    var name ="Harshit";
+    var age = 23;
+    return name + " " + age;
+}
+
+export default Test;
+
+// console.log(testFun())
+// console.log(testFun())
+// console.log(testFun())
